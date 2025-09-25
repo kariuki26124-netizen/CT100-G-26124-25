8a7ef5457657e38943b86918afa279fd1e6eb5e4
kariuki26124-netizen <kariuki.26124@students.kyu.ac.ke>
2025-09-25 23:33:19 +0300

    Add files via upload

---
+++ VOLUME AND SURFACE AREA.h
@@ -0,0 +1,34 @@
+/*
+Name:Kariuki Lewis Maina
+Reg:CT100/G/26124/25
+Description:Volume and Surface area
+S_I=P*rate/100*time
+*/
+
+#incluude<stdio.h>
+
+int main()
+{
+    //variable declaration
+    float principle,rate,time,interet
+
+    //prompt user to enter amount
+    printf("Enter principle amount:");
+    scanf("%f", &principle);
+
+    //prompt user to enter rate
+    printf("\n Enter rate:");
+    scanf("%f",&rate);
+
+    //prompt user to enter time
+    printf("\n Enter time:");
+    scanf("%f",&time);
+
+    //computer interest
+    interest=principle*rate/100*time;
+
+    //display interest
+    printf("\n Simple Interest is Ksh.%.2f",interest);
+
+    return 0
+}
