5b71d44cf35b83850ab1149312b95a7fb6c8b914
kariuki26124-netizen <kariuki.26124@students.kyu.ac.ke>
2025-09-25 19:46:11 +0300

    Add files via upload

---
+++ Units of Semester 1.h
@@ -0,0 +1,19 @@
+//simple C program
+#include <stdio.h>
+/*
+Name:Kariuki Lewis Maina
+Reg:CT100/G/26124/25
+Description:Units    for Semester 1
+*/
+int main()
+{
+    printf("SPI2107 Analogue Electronics\n");
+    printf("SPI2102 Computer Systems and Organizations\n");
+    printf("UCU2105 Fundamentals of ICT\n");
+    printf("SPI2103 Introduction to Computer Applications\n");
+    printf("SPI2120 Communication Skills\n");
+    printf("SPM2104 Mathematics for Science\n");
+    printf("SPI2101 Introduction to Programming and Algorithms\n");
+
+    return 0;
+}
