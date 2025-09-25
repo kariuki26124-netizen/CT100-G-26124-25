4bc077ca87cd1e758291578505e973458484b25b
kariuki26124-netizen <kariuki.26124@students.kyu.ac.ke>
2025-09-25 20:56:14 +0300

    Add files via upload

---
+++ USER INFORMATION.h
@@ -0,0 +1,27 @@
+//simple C program
+#include<stdio.h>
+/*
+Name:Kariuki Lewis Maina
+Reg:CT100/G/26124/25
+Description:User Information
+*/
+int main(){
+    int height;
+    int phone number;
+    int weight;
+
+    printf("Enter your height:");
+    scanf("%d"c, &height);
+
+    printf("Enter your Phone Number:");
+    scanf("%d"c, &phone number);
+
+    printf("Enter your weight:");
+    scanf("%d"c, &weight);
+
+    printf("\n Your height is %d\n",height);
+    printf("\n Your phone number is %d\n",phone number);
+    printf("\n Your weight is %d Kg\n",weight);
+
+    return 0;
+    }
